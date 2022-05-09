cd735fc476fa2a28fc6277b61f8fcb734c13eed0
carolyne <pcmaunda@gmail.com>
2022-05-09 21:13:11 +0300

    updated views file

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for,abort,request
 from . import main
 from flask_login import login_required,current_user
 from ..models import User,Pitch,Comment,Upvote,Downvote
-from  main.forms import UpdateProfile,PitchForm,CommentForm
+from  .forms import UpdateProfile,PitchForm,CommentForm
 from .. import db,photos
 
 @main.route('/')
