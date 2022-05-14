e7f82ddc0d788e78464ae04da122240fee4cb7c0
carolyne <pcmaunda@gmail.com>
2022-05-14 21:05:49 +0300

    installed sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+
 
 
 db = SQLAlchemy()
-DB_NAME ='database'
+
 
 def create_app():
     app =Flask(__name__)
 
 
     app.config['SECRET_KEY'] = '123'
-    app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI']=''
+    
+    bootstrap = Bootstrap(app)   
+ 
+
+
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
+
+    from .models import User , Pitch
+
+  
+
 
     return app
