766875bb485741b20e3e5339d9d5ff2e1778ccdb
carolyne <pcmaunda@gmail.com>
2022-05-15 10:39:13 +0300

    modified navbar

--- main.py
+++ main.py
@@ -2,54 +2,50 @@ from flask import Flask,render_template,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField,SubmitField,PasswordField
 from wtforms.validators import DataRequired
 
 
-class ShoppingForm(FlaskForm):
+class PitchForm(FlaskForm):
     item = StringField('item', validators=[DataRequired()])
     quantity = StringField('quantity', validators=[DataRequired()])
     submit = SubmitField('add')
-
-  
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY']='123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://carolyne:123@localhost/list'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://carolyne:123@localhost/pitches'
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
 
-class Shopping(db.Model):
-    __tablename__ = 'lists'
+class Pitches(db.Model):
+    __tablename__ = 'pitches'
     id = db.Column(db.Integer,primary_key = True)
     item = db.Column(db.String(255))
     amount = db.Column(db.String(150))
-    
-
 
 @app.route('/')
 def index():
-    items = Shopping.query.all()
+    items = Pitches.query.all()
     return render_template('index.html',goods=items)
 
 
-@app.route('/items',methods=['GET','POST'])   
+@app.route('/pitches',methods=['GET','POST'])   
 def items():
-    form = ShoppingForm()
+    form = PitchForm()
     if form.validate_on_submit():
-        item = Shopping(item=form.item.data, amount=form.quantity.data)
+        item = Pitches(item=form.item.data, amount=form.quantity.data)
         db.session.add(item)
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('list.html', data=form)
 
-@app.route('/delete/<int:item_id>')
+@app.route('/delete/<int:pitch_id>')
 def delete(item_id):
-    item = Shopping.query.filter_by(id=item_id).first()
+    item = Pitches.query.filter_by(id=item_id).first()
     db.session.delete(item)
     db.session.commit()
     return redirect(url_for('index'))
@@ -58,13 +54,12 @@ def delete(item_id):
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, Shopping=Shopping)
+    return dict(db=db, Pitches=Pitches)
         
 
 if __name__ == "__main__":
     
     app.run(debug=True)
-
 
         
 
